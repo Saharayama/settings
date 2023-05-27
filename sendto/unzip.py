6130fa05532dbc0edea22566657faee89ea2de7d
Saharayama <47473618+Saharayama@users.noreply.github.com>
2023-05-27 14:55:45 +0900

    update case

--- sendto/unzip.py
+++ sendto/unzip.py
@@ -2,18 +2,18 @@ from pathlib import Path
 import sys
 import subprocess
 
-suffix_list = ['.zip', '.7z']
-seven_zip = Path(r'C:\Program Files\7-Zip\7z.exe')
+SUFFIX_LIST = ['.zip', '.7z']
+SEVEN_ZIP = Path(r'C:\Program Files\7-Zip\7z.exe')
 
 for n in sys.argv[1:]:
     archive_path = Path(n)
     out_dir = Path(archive_path.parent, archive_path.stem)
-    if archive_path.suffix in suffix_list:
+    if archive_path.suffix in SUFFIX_LIST:
         if out_dir.exists():
             print(str(out_dir) + ' already exists.')
             subprocess.call('PAUSE', shell = True)
             sys.exit()
-        result = subprocess.run([seven_zip, 'x', archive_path, '-o' + str(out_dir)])
+        result = subprocess.run([SEVEN_ZIP, 'x', archive_path, '-o' + str(out_dir)])
         if result.returncode == 0:
             archive_path.unlink()
         else:
