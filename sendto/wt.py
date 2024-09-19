a8ee9dd12e8cc02847ca158632aa320d8b07f366
Saharayama <47473618+Saharayama@users.noreply.github.com>
2024-09-19 20:48:08 +0900

    update sendto wt.py

--- sendto/wt.py
+++ sendto/wt.py
@@ -2,12 +2,23 @@ import sys
 import subprocess
 from pathlib import Path
 
-for n in sys.argv[1:]:
-    path = Path(n)
-    if path.is_dir():
-        path_dir = path
-    elif path.is_file():
-        path_dir = path.parent.resolve()
-    else:
-        continue
-    subprocess.Popen([r"wt", "-d", path_dir])
+
+def get_directories(paths):
+    return {
+        str(Path(path).parent if Path(path).is_file() else Path(path)) for path in paths
+    }
+
+
+def build_command(directories):
+    command = "wt " + " ".join([f'-d "{dir_path}" ;' for dir_path in directories])
+    return command.rstrip(" ;")
+
+
+def open_windows_terminal(paths):
+    directories = get_directories(paths)
+    command = build_command(directories)
+    subprocess.Popen(command)
+
+
+if __name__ == "__main__":
+    open_windows_terminal(sys.argv[1:])
