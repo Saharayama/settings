f59fc62a289cdf1ccee4f6ebc9d7f392550721dd
Saharayama <47473618+Saharayama@users.noreply.github.com>
2026-08-26 22:44:31 +0900

    update vscode tasks

--- vscode/vscode-tasks-format-c-comment.sh
+++ vscode/vscode-tasks-format-c-comment.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-set -eu
+set -euo pipefail
 
-line_count=$(printf '%s\n' "${SELECTED_TEXT}" | wc -l)
+line_count=$(printf '%s\n' "$SELECTED_TEXT" | wc -l)
 line_start=$((LINE_NUMBER - line_count + 1))
-unix_path=$(cygpath -u "${RELATIVE_FILE}")
+unix_path=$(cygpath -u "$FILE")
+line_range="${line_start},${LINE_NUMBER}"
 
 sed -i -b -E "
-  ${line_start},${LINE_NUMBER} s_//![[:blank:]]*([^[:blank:]!\r])_//! \1_g
-  ${line_start},${LINE_NUMBER} s_///([^[:blank:]/\r])_/// \1_g
-  ${line_start},${LINE_NUMBER} s_/\*\*([^[:blank:]*<\r])_/** \1_g
-  ${line_start},${LINE_NUMBER} s_/\*!([^[:blank:]!<\r])_/*! \1_g
+  ${line_range} s_//![[:blank:]]*([^[:blank:]!\r])_//! \1_g
+  ${line_range} s_///([^[:blank:]/\r])_/// \1_g
+  ${line_range} s_/\*\*([^[:blank:]*<\r])_/** \1_g
+  ${line_range} s_/\*!([^[:blank:]!<\r])_/*! \1_g
 
-  ${line_start},${LINE_NUMBER} s_/\*[[:blank:]]*([^[:blank:]*!\r])_/* \1_g
-  ${line_start},${LINE_NUMBER} s_([^[:blank:]*])[[:blank:]]*\*/_\1 */_g
-  ${line_start},${LINE_NUMBER} s_//[[:blank:]]*([^/!\r])_// \1_g
-  ${line_start},${LINE_NUMBER} s_[[:blank:]]+(\r?)\$_\1_
-" "${unix_path}"
+  ${line_range} s_/\*[[:blank:]]*([^[:blank:]*!\r])_/* \1_g
+  ${line_range} s_([^[:blank:]*])[[:blank:]]*\*/_\1 */_g
+  ${line_range} s_//[[:blank:]]*([^/!\r])_// \1_g
+  ${line_range} s_[[:blank:]]+(\r?)\$_\1_
+" "$unix_path"
